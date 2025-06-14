1c886566ca796b0518712414d430cdc2e2abb98a
onur846 <115958771+onur846@users.noreply.github.com>
2025-06-14 18:50:36 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
 import "../styles/globals.css";
+import { WagmiConfig, createConfig, http } from "wagmi";
+import { mainnet } from "wagmi/chains";
+
+const config = createConfig({
+  chains: [mainnet], 
+  transports: {
+    [mainnet.id]: http(),
+  },
+  ssr: true,
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <WagmiConfig config={config}>
+      <Component {...pageProps} />
+    </WagmiConfig>
+  );
 }
