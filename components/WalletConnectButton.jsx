64897d4df4033d34bf5c8f322c309d994f8e3192
onur846 <115958771+onur846@users.noreply.github.com>
2025-06-14 19:10:31 +0300

    Update WalletConnectButton.jsx

--- components/WalletConnectButton.jsx
+++ components/WalletConnectButton.jsx
@@ -1,22 +1,11 @@
-"use client";
-import { useAccount, useConnect, useDisconnect } from "wagmi";
-import { InjectedConnector } from "wagmi/connectors/injected";
+import { useConnect } from 'wagmi';
+import { injected } from 'wagmi/connectors';
 
 export default function WalletConnectButton() {
-  const { address, isConnected } = useAccount();
-  const { connect } = useConnect({ connector: new InjectedConnector() });
-  const { disconnect } = useDisconnect();
-
-  if (isConnected)
-    return (
-      <button onClick={() => disconnect()} className="bg-red-600 text-white px-4 py-2 rounded-xl">
-        Disconnect ({address.slice(0, 6)}...{address.slice(-4)})
-      </button>
-    );
+  const { connect } = useConnect();
   return (
-    <button onClick={() => connect()} className="bg-green-600 text-white px-4 py-2 rounded-xl">
+    <button onClick={() => connect({ connector: injected() })}>
       Connect Wallet
     </button>
   );
 }
-
