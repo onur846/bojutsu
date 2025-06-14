ec626ae83d21a3841efe4f3b729bb129880bd29f
onur846 <115958771+onur846@users.noreply.github.com>
2025-06-14 19:16:54 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,14 @@
-import "../styles/globals.css";
-import { WagmiConfig, createConfig, http } from "wagmi";
-import { mainnet } from "wagmi/chains";
-
-const config = createConfig({
-  chains: [mainnet], 
-  transports: {
-    [mainnet.id]: http(),
-  },
-  ssr: true,
-});
+import '@/styles/globals.css'
+import { WagmiProvider } from 'wagmi'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { config, queryClient } from '../lib/wagmi' // adjust path if needed
 
 export default function App({ Component, pageProps }) {
   return (
-    <WagmiConfig config={config}>
-      <Component {...pageProps} />
-    </WagmiConfig>
-  );
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </WagmiProvider>
+  )
 }
