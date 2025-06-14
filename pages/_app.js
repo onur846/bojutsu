72b8cdf95e48d77ca812e21d4daf40409e84e251
onur846 <115958771+onur846@users.noreply.github.com>
2025-06-14 20:02:48 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,48 +1,26 @@
-import '@/styles/globals.css';
-import { WagmiProvider, createConfig, http } from 'wagmi';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import '@/styles/globals.css'
+import { WagmiProvider, createConfig, http } from 'wagmi'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import {
-  mainnet,
-  arbitrum,
-  polygon,
-  optimism,
-  base,
-  bsc,
-  avalanche,
-  fantom,
-  zora,
-  gnosis,
-  linea,
-  scroll,
-  mantle,
-} from 'wagmi/chains';
-import { InjectedConnector } from '@wagmi/connectors/injected';
+  mainnet, arbitrum, polygon, optimism, base, bsc,
+  avalanche, fantom, zora, gnosis, linea, scroll, mantle
+} from 'wagmi/chains'
+import { InjectedConnector } from 'wagmi/connectors/injected'
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient()
 
 const chains = [
-  mainnet,
-  arbitrum,
-  polygon,
-  optimism,
-  base,
-  bsc,
-  avalanche,
-  fantom,
-  zora,
-  gnosis,
-  linea,
-  scroll,
-  mantle,
-];
+  mainnet, arbitrum, polygon, optimism, base, bsc,
+  avalanche, fantom, zora, gnosis, linea, scroll, mantle
+]
 
 const wagmiConfig = createConfig({
-  connectors: [new InjectedConnector()],
   chains,
+  connectors: [new InjectedConnector({ chains })],
   transports: Object.fromEntries(
     chains.map((chain) => [chain.id, http()])
-  ),
-});
+  )
+})
 
 export default function App({ Component, pageProps }) {
   return (
@@ -51,5 +29,5 @@ export default function App({ Component, pageProps }) {
         <Component {...pageProps} />
       </QueryClientProvider>
     </WagmiProvider>
-  );
+  )
 }
