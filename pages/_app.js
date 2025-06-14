31bc901e95940e434d816f773f470f9c3e9f76f7
onur846 <115958771+onur846@users.noreply.github.com>
2025-06-14 19:45:18 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,15 @@
-import '../styles/globals.css'
-import { WagmiProvider } from 'wagmi'
-import { QueryClientProvider } from '@tanstack/react-query'
-import { config, queryClient } from '../lib/wagmi' // adjust path if needed
+import "@/styles/globals.css";
+import WalletConnectButton from "@/components/WalletConnectButton";
 
 export default function App({ Component, pageProps }) {
   return (
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
-      </QueryClientProvider>
-    </WagmiProvider>
-  )
+    <div>
+      {/* Top Bar */}
+      <div className="w-full flex justify-end items-center px-6 py-4 border-b" style={{position: "sticky", top: 0, zIndex: 20, background: "#fff"}}>
+        <WalletConnectButton />
+      </div>
+      {/* Page Content */}
+      <Component {...pageProps} />
+    </div>
+  );
 }
