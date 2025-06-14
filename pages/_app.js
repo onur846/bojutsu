5f9570c20f6f5d5771da30cc454171f4963faa99
onur846 <115958771+onur846@users.noreply.github.com>
2025-06-14 19:46:53 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import "../styles/globals.css";
-import WalletConnectButton from "@/components/WalletConnectButton";
+import WalletConnectButton from "../components/WalletConnectButton";
 
 export default function App({ Component, pageProps }) {
   return (
