4645bfec3405d69b38ce18e70e099a51a099ff74
onur846 <115958771+onur846@users.noreply.github.com>
2025-06-14 19:58:20 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,55 @@
-import "../styles/globals.css";
-import WalletConnectButton from "../components/WalletConnectButton";
+import '@/styles/globals.css';
+import { WagmiProvider, createConfig, http } from 'wagmi';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import {
+  mainnet,
+  arbitrum,
+  polygon,
+  optimism,
+  base,
+  bsc,
+  avalanche,
+  fantom,
+  zora,
+  gnosis,
+  linea,
+  scroll,
+  mantle,
+} from 'wagmi/chains';
+import { InjectedConnector } from 'wagmi/connectors/injected';
+
+const queryClient = new QueryClient();
+
+const chains = [
+  mainnet,
+  arbitrum,
+  polygon,
+  optimism,
+  base,
+  bsc,
+  avalanche,
+  fantom,
+  zora,
+  gnosis,
+  linea,
+  scroll,
+  mantle,
+];
+
+const wagmiConfig = createConfig({
+  connectors: [new InjectedConnector()],
+  chains,
+  transports: Object.fromEntries(
+    chains.map((chain) => [chain.id, http()])
+  ),
+});
 
 export default function App({ Component, pageProps }) {
   return (
-    <div>
-      {/* Top Bar */}
-      <div className="w-full flex justify-end items-center px-6 py-4 border-b" style={{position: "sticky", top: 0, zIndex: 20, background: "#fff"}}>
-        <WalletConnectButton />
-      </div>
-      {/* Page Content */}
-      <Component {...pageProps} />
-    </div>
+    <WagmiProvider config={wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </WagmiProvider>
   );
 }
