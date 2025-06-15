1f4964db9fa2d16833d02bc0da40df451c984c71
onur846 <115958771+onur846@users.noreply.github.com>
2025-06-15 16:29:37 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,33 @@
-import { WagmiProvider } from 'wagmi'
+import '../styles/globals.css'
+import { WagmiConfig, createConfig, http } from 'wagmi'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { config } from '../lib/wagmi'
+import {
+  mainnet, arbitrum, polygon, optimism, base, bsc,
+  avalanche, fantom, zora, gnosis, linea, scroll, mantle
+} from 'wagmi/chains'
+import { injected } from 'wagmi/connectors'
 
 const queryClient = new QueryClient()
 
+const chains = [
+  mainnet, arbitrum, polygon, optimism, base, bsc,
+  avalanche, fantom, zora, gnosis, linea, scroll, mantle
+]
+
+const wagmiConfig = createConfig({
+  chains,
+  connectors: [injected()],
+  transports: Object.fromEntries(
+    chains.map((chain) => [chain.id, http()])
+  )
+})
+
 export default function App({ Component, pageProps }) {
   return (
-    <WagmiProvider config={config}>
+    <WagmiConfig config={wagmiConfig}>
       <QueryClientProvider client={queryClient}>
         <Component {...pageProps} />
       </QueryClientProvider>
-    </WagmiProvider>
+    </WagmiConfig>
   )
 }
