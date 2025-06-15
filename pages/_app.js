0a64675fa423d43d5d6b7ce167653b3d7a4b907c
onur846 <115958771+onur846@users.noreply.github.com>
2025-06-15 14:46:03 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,33 +1,12 @@
-import '../styles/globals.css'
-import { WagmiProvider, createConfig, http } from 'wagmi'
+import { WagmiProvider } from 'wagmi'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import {
-  mainnet, arbitrum, polygon, optimism, base, bsc,
-  avalanche, fantom, zora, gnosis, linea, scroll, mantle
-} from 'wagmi/chains'
-import { injected, walletConnect } from 'wagmi/connectors' 
+import { config } from '../lib/wagmi'
 
-const chains = [mainnet, arbitrum, polygon, optimism, base, bsc, avalanche, fantom, zora, gnosis, linea, scroll, mantle];
-
-// You forgot this line:
-const queryClient = new QueryClient();
-
-const wagmiConfig = createConfig({
-  chains,
-  connectors: [
-    injected(),
-    walletConnect({
-      projectId: '39681bbf49eacfab90129b7821dc5a52',
-    }),
-  ],
-  transports: Object.fromEntries(
-    chains.map((chain) => [chain.id, http()])
-  )
-})
+const queryClient = new QueryClient()
 
 export default function App({ Component, pageProps }) {
   return (
-    <WagmiProvider config={wagmiConfig}>
+    <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
         <Component {...pageProps} />
       </QueryClientProvider>
